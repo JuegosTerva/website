b6092bffbd8eab8bb7380458ae9ef8b640691a1f
JuegosTerva <juegosterva@gmail.com>
2025-06-15 23:06:49 -0600

    v25.06.05

---
+++ descargas/MovimientoJugador4Direcciones.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    public float velocidad = 5f;
+    Rigidbody2D rb;
+    public Vector2 entrada;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = entrada * velocidad;
+    }
+
+    public void Moverse(InputAction.CallbackContext contexto){
+        
+        Vector2 valorEntrada = contexto.ReadValue<Vector2>();
+
+        // Determinar el eje dominante
+        if (Mathf.Abs(valorEntrada.x) > Mathf.Abs(valorEntrada.y))
+        {
+            // Movimiento horizontal
+            entrada = new Vector2(Mathf.Sign(valorEntrada.x), 0);
+        }
+        else if (Mathf.Abs(valorEntrada.y) > 0)
+        {
+            // Movimiento vertical
+            entrada = new Vector2(0, Mathf.Sign(valorEntrada.y));
+        }
+        else
+        {
+            entrada = Vector2.zero;
+        }
+    }
+}
